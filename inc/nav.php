235ff36e550a83f51aff458edf63e491653c8048
faith-egbon <osagiodagbonfaith.egbon@gmail.com>
2022-11-10 09:38:47 +0100

    message

---
+++ inc/nav.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Boootstrap CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossOrigin="anonymous">
+    <!-- Bootstrap Icon CDN -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+</head>
+<body>
+
+<style>
+  nav {
+    background: #04519bb3;
+    z-index: 0;
+  }
+  @media screen and (max-width: 500px) {
+    body {
+      overflow-x: hidden;
+    }
+  }
+  body{
+    background-color:ghostwhite;
+  }
+</style>
+
+<!-- <?php session_start(); ?> -->
+
+  <!-- navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark py-3 fixed-top">
+    <div class="container">
+      <!-- navbar brand / title -->
+      <a class="navbar-brand" href="index.php">
+        <span class="text-light fw-bold">
+          <h3 class="m-0">
+            <!-- <i class="bi bi-coin"></i> -->
+            IM-System
+          </h3>
+        </span>
+      </a>
+      <!-- toggle button for mobile nav -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- navbar links -->
+      <div class="collapse navbar-collapse justify-content-end align-center" id="main-nav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link text-light ms-lg-3 ms-0" href="index.php#home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light ms-lg-3 ms-0" href="index.php#about">About</a>
+          </li>
+          <!-- <li class="nav-item">
+            <a class="nav-link text-light ms-lg-3 ms-0" href="index.php#web_technologies">Web Technologies</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light ms-lg-3 ms-0" href="index.php#team">Team</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light ms-lg-3 ms-0" href="index.php#faq">FAQ</a>
+          </li> -->
+          <li class="nav-item">
+            <a class="nav-link text-light ms-lg-3 ms-0" href="index.php#contact">Contact</a>
+          </li>
+          <li class="nav-item d-lg-none d-flex">
+
+          <?php if(isset($_SESSION['user_id'])==false): ?>
+            <a class="nav-link btn btn-primary px-3" href="login.php">Log In</a>
+            <a class="nav-link btn btn-primary px-3 ms-2" href="register.php">Register</a>
+          <?php else: ?>
+            <a class="nav-link btn btn-primary px-3" href="dashboard.php">Dashboard</a>
+            <a class="nav-link btn btn-primary px-3 ms-2" href="logout.php">Logout</a>
+          <?php endif; ?>
+
+
+          <?php if(isset($_SESSION['user_id'])==false): ?>
+          <li class="nav-item ms-2 d-none d-lg-inline" id="toggleshow">
+            <a class="btn btn-primary" href="login.php">Log In</a>
+          </li>
+          <li class="nav-item ms-2 d-none d-lg-inline" id="toggleshow">
+            <a class="btn btn-primary" href="register.php">Register</a>
+          </li>
+          <?php else: ?>
+            <li class="nav-item ms-2 d-none d-lg-inline" id="toggleshow">
+            <a class="btn btn-primary" href="dashboard.php">Dashboard</a>
+          </li>
+          <li class="nav-item ms-2 d-none d-lg-inline" id="toggleshow">
+            <a class="btn btn-primary" href="logout.php">Logout</a>
+          </li>
+          <?php endif; ?>
+        </ul>
+      </div>
+    </div>
+  </nav>
+            
---
+++ inc/navigate.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
+    <title>InventoryCo | Landing Page</title>
+</head>
+<body>
+  <style>
+    nav{
+      height: 80px;
+    }
+  </style>
+  <nav class="navbar navbar-expand-lg navbar-light sticky-top" style="background-image: none; background-color: #04519bb3;">
+    <div class="container">
+      <a class="navbar-brand" href="index.php"><img src="image/Ico.png" height="70" alt="logo"></a><button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"> </span></button>
+      <div class="collapse navbar-collapse  mt-4 mt-lg-0" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item"><a class="nav-link" aria-current="page" href="about.php">About</a></li>
+          <li class="nav-item"><a class="nav-link" aria-current="page" href="#">Contact</a></li>
+          <li class="nav-item"><a class="nav-link" aria-current="page" href="#">FAQ</a></li>
+        </ul>
+        <div class="d-flex ms-lg-4">
+          <a class="btn button btn-secondary-outline" href="login.php">Login</a>
+          <a class="btn button ms-3" href="login.php?action=register">Register</a></div>
+      </div>
+    </div>
+  </nav>
